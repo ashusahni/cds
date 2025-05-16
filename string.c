2453cb5b72f684809afedd405504d519784e7f87
ashutosh <ashutoshsahni833@gmail.com>
2025-05-17 04:20:02 +0530

    iteration

---
+++ string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (void){
+    int num[]= {34,434,650,50,40,100,540};
+    int n; 
+    printf("number :");
+    scanf("%d", &n);
+
+    for (int i = 0; i<7; i++){
+        if (num[i] == n){
+            printf("number found");
+            return 0;
+        }
+    }
+       {
+            printf("not fund");
+            return 1;
+        }
+
+    // printf("u enter : %d\n", n);
+
+}
